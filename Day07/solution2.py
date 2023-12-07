f70a62e60e4a9f4f1dee89c768295acfb8c34351
“Krzysztof <krzysztof.gebka2001@gmail.com>
2023-12-07 10:51:46 +0100

    Solution for day 7

---
+++ Day07/solution2.py
@@ -0,0 +1,91 @@
+class Card:
+    def __init__(self, label) -> None:
+            self.strength = 0
+            match label:
+                case 'A':
+                    self.strength = 13
+                case 'K':
+                    self.strength = 12
+                case 'Q':
+                    self.strength = 11
+                case 'J':
+                    self.strength = -1
+                case 'T':
+                    self.strength = 9
+                case '9':
+                    self.strength = 8
+                case '8':
+                    self.strength = 7
+                case '7':
+                    self.strength = 6
+                case '6':
+                    self.strength = 5
+                case '5':
+                    self.strength = 4
+                case '4':
+                    self.strength = 3
+                case '3':
+                    self.strength = 2
+                case '2':
+                    self.strength = 1
+
+class Hand:
+    def __init__(self, cards, bid) -> None:
+        self.cards = cards
+        self.bid = bid
+        sorted_cards = sorted(cards, key=lambda x: x.strength)
+        count_list = []
+        count = 1
+        counting_card = sorted_cards[0]
+        count_joker = 0
+        if sorted_cards[0].strength == -1:
+            count_joker += 1
+        for card in sorted_cards[1:]:
+            if counting_card.strength == card.strength:
+                count += 1
+                if card.strength == -1:
+                    count_joker += 1
+            else:
+                counting_card = card
+                count_list.append(count)
+                count = 1
+        count_list.append(count)
+
+        count_list.sort()
+        if count_joker != 0:
+            count_list.remove(count_joker)
+        if count_list == [5] or (count_joker == 1 and count_list == [4]) or (count_joker == 2 and count_list == [3]) or (count_joker == 3 and count_list == [2]) or (count_joker == 4 and count_list == [1]) or (count_joker == 5 and count_list == []):
+            self.type = 7
+        elif count_list == [1, 4] or (count_joker == 1 and count_list == [1, 3]) or (count_joker == 2 and count_list == [1, 2]) or (count_joker == 3 and count_list == [1, 1]):
+            self.type = 6
+        elif count_list == [2, 3] or (count_joker == 1 and count_list == [2, 2]):
+            self.type = 5
+        elif count_list == [1, 1, 3] or (count_joker == 1 and count_list == [1, 1, 2]) or (count_joker == 2 and count_list == [1, 1, 1]):
+            self.type = 4
+        elif count_list == [1, 2, 2]:
+            self.type = 3
+        elif count_list == [1, 1, 1, 2] or (count_joker == 1 and count_list == [1, 1, 1, 1]):
+            self.type = 2
+        else:
+            self.type = 1
+            
+def hand_sort_key(hand):
+    return (hand.type, hand.cards[0].strength, hand.cards[1].strength, hand.cards[2].strength, hand.cards[3].strength, hand.cards[4].strength)
+
+with open('Day07/input.in', 'r') as file:
+    lines = file.read().splitlines()
+
+hands = []
+for line in lines:
+    hand_and_dip_as_string = line.split()
+    cards = [Card(x) for x in hand_and_dip_as_string[0]]
+    bid = int(hand_and_dip_as_string[1])
+    hand = Hand(cards, bid)
+    hands.append(hand)
+
+hands.sort(key=hand_sort_key)
+
+result = 0
+for i, v in enumerate(hands):
+    result += (i+1)*v.bid
+print(result)
